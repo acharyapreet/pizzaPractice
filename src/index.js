96dbb645e4935b5c9091c70bd97f614bd7bec0f4
Preet Acharya <acharyapreet2@gmail.com>
2025-03-08 18:39:03 +0530

    connect to database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const serverConfig = require('./config/serverConfig');
+const connectDB = require('./config/dbConfig');
 
 // making a app object of express
 const app = express();
 
+//these are deserilizers
+//used to read/parse the incoming request
+app.use(bodyParser.json());
+//extended true means we can parse nested objects and it remove the warning
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.text());
+
+app.post('/ping', (req, res) => {
+    console.log(req.body);
+    return res.json({message : "pong"})
+})
+
 //starting the server
-app.listen(serverConfig.PORT, () => {
-    console.log(`server is running on port ${serverConfig.PORT}` );
+app.listen(serverConfig.PORT, async() => {
+    await connectDB();
+    console.log(`server is running on port ${serverConfig.PORT}`);
 }) 
